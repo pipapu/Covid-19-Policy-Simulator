390f3064310c204ca8488abfc595df8f49df5557
Axel G. Rossberg <axel@rossberg.net>
2020-03-21 16:51:02 +0000

    Policy now taking current growth rate into account.

--- Covid-19-Policy-Simulator.R
+++ Covid-19-Policy-Simulator.R
@@ -27,8 +27,10 @@ stages <- c(
   c() )
 n_stages <- length(stages)
 
-# Infectious stages
+# Infected stages
 IStages <- c("Is1", "Is2", "Ia1", "Ia2")
+# Infectious (test postive) stages
+PStages <- c("Is2", "Ia2")
 
 
 ## Linear dynamics:
@@ -112,7 +114,9 @@ L <- SS + FF
   
 IStages <- c("Is1", "Is2", "Ia1", "Ia2")
 #get_mean_generation_time(SS[IStages,IStages],FF[IStages,IStages])
-log(eigen(L[IStages,IStages])$values[1]) # linear growth rate r should be around 0.2
+linear_growth_rate <- # linear growth rate r should be around 0.2
+  log(eigen(L[IStages,IStages])$values[1]) 
+linear_growth_rate
 
 # Non-linear dynamics
 n_steps <- 365
@@ -121,9 +125,6 @@ colnames(state) <- stages
 S_index <- which(stages == "S")
 state[1,"Ia1"] <- 100
 state[1,"S"] <- pop_size - state[1,"Ia1"]
-dfac <- rep(NA,n_steps+1) # social distancing factor, the controll parameter
-dfac[1] <- 1 # social distancing factor, the controll parameter
-line_alpha <- 255 # for transparency choice
 
 ## Simulate without policy interventions
 plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),xlab="Days",ylab="Proportion")
@@ -139,24 +140,29 @@ legend(x="bottomright",
        col=c("black","red"),lty=1,bg=rgb(1,1,1,alpha = 0.4))
 
 ## Simulate policy interventions with uncertainty
-n_runs <- 10
-other_runs_transparency <- 0.1
+dfac <- rep(NA,n_steps+1) # social distancing factor, the controll parameter
+dfac[1] <- 1 # social distancing factor, the controll parameter
+growth_rate <- linear_growth_rate
+n_runs <- 200
+other_runs_transparency <- 0.05
 # plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),ylab="Proportion immunized",xlab="Days")
 # plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),ylab="Social Distancing Factor",xlab="Days")
 # plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),ylab="Proportion infected",xlab="Days")
 plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),xlab="Days",ylab="Proportion")
 for(run in 1:n_runs){
   for(i in 1:n_steps){
-    dfac[i+1] <- pnorm(qnorm(dfac[i])+rnorm(1,1/30,1/30)) # decline of compliance
-    if(i %% 7 == 0){# Every week, adjust policy:
-      if(state[i,"Is2"]/pop_size > 0.005) # more measures
+    dfac[i+1] <- pnorm(qnorm(dfac[i])+rnorm(1,1/60,1/30)) # decline of compliance
+    #if(i %% 7 == 0){# Every week, adjust policy:
+    if(runif(1)< 1/7){# Approximately every week, adjust policy:
+      if(state[i,"Is2"]/pop_size > 0.001 && growth_rate > 0) # more measures
         dfac[i+1] <- dfac[i+1] * runif(1,0.2,1) # outcome is uncertain
-      else if(state[i,"Is2"]/pop_size < 0.005) # less measures
+      else if(state[i,"Is2"]/pop_size < 0.001) # less measures
         dfac[i+1] <- dfac[i+1]*min(1,runif(1,1,1.5)) # outcome uncertain
     }
     Lx <- SS + FF0 * state[i,"S"] * dfac[i+1]
     state[i+1,] <- Lx %*% state[i,]
     state[i+1,"S"] <- pop_size - sum(state[i+1,-S_index])
+    growth_rate <- log(sum(state[i+1,PStages])/sum(state[i,PStages]))
   }
   line_alpha <- ifelse(run==1,1,other_runs_transparency)*255
   plotcol <- do.call(rgb,as.list(c(col2rgb("black"),alpha=line_alpha,max = 255)))
