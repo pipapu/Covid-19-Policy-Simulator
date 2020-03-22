a9cc128441bec6979ac7bcfddcfcf816a330aa84
Axel G. Rossberg <axel@rossberg.net>
2020-03-22 15:44:59 +0000

    Now with a prototype of a mortality model taking bed capacity overloads into account.

--- Covid-19-Policy-Simulator.R
+++ Covid-19-Policy-Simulator.R
@@ -2,25 +2,36 @@
 library(nleqslv) # nonlinear solver
 
 # Parameters (unit: days)
-incubation_time <- 5.1# Ferguson et al. 
+incubation_time <- 5.1 # Ferguson et al. 
+incubation_time <- incubation_time +1 # fudge to reproduce R0 and generation time 
 start_trans_symp <- incubation_time - 0.5 # Ferguson et al. 
 start_trans_asymp <- start_trans_symp # Ferguson et al. 
 prop_symptoms <- 0.2 # https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)30567-5/fulltext
 #prop_symptoms <- 1/3 # Ferguson et al. 
 prop_critical <- 0.06 # To be implemented # https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)30567-5/fulltext
 mean_generation_time <- 6.5 # Ferguson et al. 
-#generation_time_alpha <- 1 # shape parameter of an assumed gamma distribution, https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6670001/
-generation_time_alpha <- 3 # shape parameter of an assumed gamma distribution, https://academic.oup.com/biostatistics/article/12/2/303/280518
-generation_time_alpha <- 1 # 1 # shape parameter of an assumed gamma distribution, fudge to reproduce r
+#generation_time_alpha <- 3 # shape parameter of an assumed gamma distribution, https://academic.oup.com/biostatistics/article/12/2/303/280518
+generation_time_alpha <- 2 # 1 # shape parameter of an assumed gamma distribution, fudge to reproduce r
 R0 <- 2.4 # Ferguson et al. 
 inf_symp_asymp_ratio <- 2  # Ferguson et al.  
 UK_population_size <- 66.44e6
 pop_size <- UK_population_size
-case_fatality_rate <- 0.01 # to be implemented...
+
+case_fatality_rate_1 <- 0.009 # hospitals below capacity # to be implemented...
+case_fatality_rate_2 <- 0.09 # hospitals over capacity # to be implemented...
+hospitalization_rate <- 0.044 # to be implemented...
+critical_care_rate <- 0.044 * 0.3 # to be implemented...
+critical_care_hospital_time <- 10
+hospital_load_factor <- 
+  critical_care_rate * critical_care_hospital_time / 
+  mean_generation_time
+critical_care_beds <- 8175 # uk number of respirators 
 
 ## Check:
 r <- (R0^(1/generation_time_alpha)-1) * generation_time_alpha/mean_generation_time;
+# linear growth rate
 r
+# doubling time
 #exp(r*T_double)==2 -> T_double == log(2)/r
 log(2)/r
 
@@ -91,12 +102,19 @@ get_mean_generation_time <- function(SS,FF){
   # See page 842 of Bienvenu, F.; Legendre, S. (2015). "A New Approach to the Generation Time in Matrix Population Models" (PDF). The American Naturalist. 185 (6): 834–843. doi:10.1086/681104. PMID 25996867.
   L <- SS+FF
   eig <- eigen(L)
-  print(eig$values)
   i <- eigenvalue_index(eig$values)
-  print(i)
   w <- eig$vectors[,i]
-  v <- eigen(t(L))$vectors[,i]
-  if(abs(t(v) %*% w)<0.001) stop("Eigenvector index mixup")
+  t_eig <- eigen(t(L))
+  j <- min(which(abs(t_eig$values - eig$values[i])<1e-10))
+  v <- t_eig$vectors[,j]
+  if(abs(t(v) %*% w)<1e-10) {
+    print(c(i,j))
+    print(eig$values)
+    print(t_eig$values)
+    print(w)
+    print(v)
+    stop("Potential eigenvector index mixup")
+  }
   T_gen <- 1 + (t(v) %*% SS %*% w) / (t(v) %*% FF %*% w) 
   return(T_gen)
 }
@@ -105,9 +123,7 @@ R0_and_Tgen <- function(SS,FF){
   L <- SS + FF
   Tgen <- get_mean_generation_time(SS,FF)
   eig <- eigen(L[IStages,IStages])
-  print(eig$values)
   i <- eigenvalue_index(eig$values)
-  print(i)
   r <- log(eig$values[i])
   # https://beast.community/estimating_R0
   # https://www.sciencedirect.com/science/article/pii/S1755436518300847
@@ -123,19 +139,20 @@ R0_and_Tgen_diff <- function(x){
            c(R0,mean_generation_time) ))
 }
 initialGuess <- c(Is2_inf=5.234017e-07/20,rec_rate=0.01 )
-with(as.list(initialGuess),
-     SS_calc(Is2_inf,rec_rate)+FF_calc(Is2_inf,rec_rate))
-with(as.list(initialGuess),
-     R0_and_Tgen(SS_calc(Is2_inf,rec_rate),FF_calc(Is2_inf,rec_rate)))
-with(as.list(initialGuess),
-     get_mean_generation_time(SS_calc(Is2_inf,rec_rate),FF_calc(Is2_inf,rec_rate)))
-#Test
+# #Tests
+# with(as.list(initialGuess),
+#      SS_calc(Is2_inf,rec_rate)+FF_calc(Is2_inf,rec_rate))
+# with(as.list(initialGuess),
+#      get_mean_generation_time(SS_calc(Is2_inf,rec_rate),FF_calc(Is2_inf,rec_rate)))
+# with(as.list(initialGuess),
+#      R0_and_Tgen(SS_calc(Is2_inf,rec_rate),FF_calc(Is2_inf,rec_rate)))
 R0_and_Tgen_diff(initialGuess)
 
+## Find parameters to fit R0 and generation time
 fit <- nleqslv(initialGuess,R0_and_Tgen_diff);
-fit$message
+fit$message  # should be "Function criterion near zero" or "x-values within tolerance 'xtol'"
 fit$x
-R0_and_Tgen_diff(fit$x)
+## double check
 with(as.list(fit$x),
      R0_and_Tgen(SS_calc(Is2_inf,rec_rate),FF_calc(Is2_inf,rec_rate)))
 
@@ -158,6 +175,21 @@ colnames(state) <- stages
 S_index <- which(stages == "S")
 state[1,"Ia1"] <- 100
 state[1,"S"] <- pop_size - state[1,"Ia1"]
+deaths <- rep(NA,n_steps+1) # deaths per step
+deaths[1] <- 0
+
+compute_deaths <- function(state_snippet){
+  total_infected <- sum(state_snippet[1,IStages])
+  respirator_demand <- hospital_load_factor * total_infected
+  with_respirator <- min(critical_care_beds, respirator_demand)
+  without_respirator <- respirator_demand - with_respirator
+  fatality_rate <-
+    (with_respirator * case_fatality_rate_1 + without_respirator * case_fatality_rate_2)/
+    respirator_demand
+  rec <- state_snippet[2,"R"] - state_snippet[1,"R"]
+  deaths <- rec * fatality_rate
+  return(deaths)
+}
 
 ## Simulate without policy interventions
 plot(c(0),type="n",ylim=c(0,1),xlim=c(0,n_steps),xlab="Days",ylab="Proportion")
@@ -165,12 +197,14 @@ for(i in 1:n_steps){
   Lx <- SS + FF0 * state[i,"S"]
   state[i+1,] <- Lx %*% state[i,]
   state[i+1,"S"] <- pop_size - sum(state[i+1,-S_index])
+  deaths[i+1] <- compute_deaths(state[c(0,1)+i,])
 }
 lines(state[,"R"]/pop_size,col="black",type="l")
 lines(rowSums(state[,IStages])/pop_size,col="red",type="l")
+lines(cumsum(deaths)/pop_size*1e1,col="brown",type="l")
 legend(x="bottomright",
-       legend = c("Recovered","Infected"),
-       col=c("black","red"),lty=1,bg=rgb(1,1,1,alpha = 0.4))
+       legend = c("Recovered","Infected","cum. mortality x 10"),
+       col=c("black","red","brown"),lty=1,bg=rgb(1,1,1,alpha = 0.4))
 
 ## Simulate policy interventions with uncertainty
 dfac <- rep(NA,n_steps+1) # social distancing factor, the controll parameter
@@ -198,17 +232,20 @@ for(run in 1:n_runs){
     Lx <- SS + FF0 * state[i,"S"] * dfac[i+1]
     state[i+1,] <- Lx %*% state[i,]
     state[i+1,"S"] <- pop_size - sum(state[i+1,-S_index])
+    deaths[i+1] <- compute_deaths(state[c(0,1)+i,])
     growth_rate <- log(sum(state[i+1,PStages])/sum(state[i,PStages]))
   }
-  line_alpha <- ifelse(run==n_runs,1,other_runs_transparency)*255
-  plotcol <- do.call(rgb,as.list(c(col2rgb("black"),alpha=line_alpha,max = 255)))
+  line_alpha <- ifelse(run > n_runs-3,1,other_runs_transparency)*255
+  plotcol <- do.call(rgb,as.list(c(col2rgb("green"),alpha=line_alpha,max = 255)))
   lines(state[,"R"]/pop_size,col=plotcol,type="l")
   plotcol <- do.call(rgb,as.list(c(col2rgb("blue"),alpha=line_alpha,max = 255)))
   lines(1-dfac,col=plotcol,type="l")
   plotcol <- do.call(rgb,as.list(c(col2rgb("red"),alpha=line_alpha,max = 255)))
   lines(rowSums(state[,IStages])/pop_size,col=plotcol,type="l")
+  plotcol <- do.call(rgb,as.list(c(col2rgb("black"),alpha=line_alpha,max = 255)))
+  lines(cumsum(deaths)/pop_size * 1e1,col=plotcol,type="l")
 }
-legend(x="bottomright",
-       legend = c("Recovered","Distancing","Infected"),
-       col=c("black","blue","red"),lty=1,bg=rgb(1,1,1,alpha = 0.4))
+legend(x="topleft",
+       legend = c("Recovered","Distancing","Infected","cum. mort. x 10"),
+       col=c("green","blue","red","black"),lty=1,bg=rgb(1,1,1,alpha = 0.4))
 
